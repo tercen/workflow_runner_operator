645d9bbc5b39ae6cb38706a073f9dc6f00d5f236
Thiago Monteiro <tsmonteiro@gmail.com>
2023-07-19 17:14:46 +0000

    file cleanup

--- main.py
+++
@@ -1,9 +0,0 @@
-from tercen.client import context as ctx
-import numpy as np
-from operator_funcs import calc_mean
-
-tercenCtx = ctx.TercenContext()
-
-df = calc_mean(tercenCtx)
-df = tercenCtx.add_namespace(df) 
-resDf = tercenCtx.save(df)

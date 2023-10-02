4a256d3295a8a98025fb17f50748758ea8ffa9b6
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-02 14:16:34 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='template_runner',
+    version='0.0.1',
+    packages=find_packages(exclude=['workflow_files']),
+    install_requires=[
+        'tercen_python_client @ git+git+https://github.com/tercen/tercen_python_client@0.7.14'
+    ],
+
+
+)

abbc81996cdbf3e70ec1c660c38894d0161f2afd
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-02 14:27:01 +0000

    client

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['workflow_files']),
     install_requires=[
-        'tercen_python_client @ git+https://github.com/tercen/tercen_python_client@0.7.15'
+        'git+https://github.com/tercen/tercen_python_client@0.7.15'
     ],
 
 

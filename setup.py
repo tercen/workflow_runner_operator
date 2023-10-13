8112cd1de5901e5d0e5e0d9a80c9d797e8721d4e
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-13 18:38:55 +0000

    module name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='template_runner',
+    name='workflow_runner',
     version='0.0.1',
     packages=find_packages(exclude=['workflow_files']),
     install_requires=[

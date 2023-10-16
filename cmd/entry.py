4856b97b1dc0a2439f1f978c1d876333e26fdc44
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-16 11:24:13 +0000

    test

---
+++ cmd/entry.py
@@ -0,0 +1,7 @@
+from template_tester import *
+
+
+
+def main():
+    run(sys.argv[1:])
+
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
     "console_scripts": [
-      "template_test = workflow_runner.entry:exec"
+      "template_test = cmd.entry:main"
     ]
     }
 )

895bd077b69f73c5454978fa77d13b5b6f24ba57
test <test@tercen.com>
2023-12-26 13:15:05 +0000

    base path

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name='workflow_runner',
     version='0.1.0',
     #packages=find_packages(exclude=['workflow_files']),
-    #packages=["workflow_funcs"],
-    #package_dir={'workflow_funcs': 'workflow_funcs' },
+    packages=["workflow_funcs", "workflow_runner"],
+    package_dir={'workflow_funcs': 'workflow_funcs', "workflow_runner":'.' },
     install_requires=[
         'tercen_python_client @ git+https://github.com/tercen/tercen_python_client@0.9.3'
     ],

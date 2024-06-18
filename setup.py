2ab17817b37c2d636d2058fc0723fa69576e614f
Thiago Monteiro <tsmonteiro@gmail.com>
2024-06-18 13:41:44 +0000

    change project name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup #, find_packages
 
 
 setup(
-    name='workflow_runner',
+    name='workflow_runner_operator',
     version='0.1.0',
     #packages=find_packages(exclude=['workflow_files']),
     packages=["workflow_funcs", "workflow_runner"],
